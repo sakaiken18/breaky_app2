924d09db79216d7c00c20fed640040d167ce47ff
sakaiken18 <sakaiken6@gmail.com>
2022-05-30 22:00:05 +0900

    mini_magickをインストール・画像サイズを変更

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -20,9 +20,10 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
-
+  
+  
   # Process files as they are uploaded:
-  # process scale: [200, 300]
+  process resize_to_limit: [200, 300]
   #
   # def scale(width, height)
   #   # do something
