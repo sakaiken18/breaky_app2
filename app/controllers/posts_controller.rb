a74ef7b4f798595e9be165f367d2bfe06f32a243
sakaiken18 <sakaiken6@gmail.com>
2022-06-09 16:59:40 +0900

    カレンダーに自分の投稿のみ表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show]
 
   def index
-    @posts = Post.order(id: :asc)
+    @posts = Post.where(user_id: current_user.id).includes(:user).all
     @post = Post.new
   end
 
