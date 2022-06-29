01fb941680bffc7530cadc56824f7d259246f079
sakaiken18 <sakaiken6@gmail.com>
2022-06-29 23:14:21 +0900

    commit

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -23,7 +23,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   
   
   # Process files as they are uploaded:
-  process resize_to_fit: [400, 400]
+  process resize_to_fit: [400, 350]
   #
   # def scale(width, height)
   #   # do something
