8a5058712c8d9405e9662fc03008305effbaec33
sakaiken18 <sakaiken6@gmail.com>
2022-07-16 22:38:52 +0900

    ゲストユーザーのパスワード再設定機能を止める

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'mypage/show'
   root 'top#index'
   devise_for :users, controllers: {
-    registrations: "users/registrations"
+    registrations: "users/registrations",
+    passwords: "users/passwords"
   }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
