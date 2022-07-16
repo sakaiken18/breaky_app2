56b853feb3b94621337ff4b5e3eef0e03708f327
sakaiken18 <sakaiken6@gmail.com>
2022-07-16 16:21:27 +0900

    ゲストログイン機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   get 'mypage/show'
   root 'top#index'
   devise_for :users
+  devise_scope :user do
+    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+  end
   get 'users/show'
   resources :posts
   resources :calendars
   resource :users, only: :show
   resource :mypage, only: :show
   resource :top, only: :index
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
