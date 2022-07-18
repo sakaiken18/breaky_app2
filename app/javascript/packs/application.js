008127408311a7d5107f52369f6a3906cd586290
sakaiken18 <sakaiken6@gmail.com>
2022-07-18 10:43:36 +0900

    ナビバーが閉じるように修正

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,16 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+ActiveStorage.start()
+
 /*!
 * Start Bootstrap - Agency v7.0.11 (https://startbootstrap.com/theme/agency)
 * Copyright 2013-2022 Start Bootstrap
