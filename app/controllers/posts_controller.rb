a0b80fccafe8acccab739ec3e96b740cbb3e5367
sakaiken18 <sakaiken6@gmail.com>
2022-09-21 22:07:24 +0900

    navbarに投稿一覧を追加・タイトルを必須項目に変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,11 +14,12 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = current_user.posts.create!(post_params)
+    @post = current_user.posts.new(post_params)
     if @post.save
       redirect_to post_path(@post)
     else
-      render "posts/index"
+      flash.now[:alert] = "投稿に失敗しました"
+      render "posts/new"
     end
     
   end
