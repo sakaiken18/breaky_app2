3d5d63cf81e4d7e6a159a51f6521620dc88533d3
sakaiken18 <sakaiken6@gmail.com>
2022-09-22 22:01:07 +0900

    ログインしていないユーザーをログイン画面へ繊維

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,4 +1,6 @@
 class MypageController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     @posts = Post.where(user_id: current_user.id).order("start_time").page(params[:page]).per(9)
   end
